4ba89ce37202ebed6aa767771af1367a7bbefd95
emilywilbur <ewilbur94@gmail.com>
2022-01-25 14:24:23 -0500

    initialed hero slider

--- index.js
+++ index.js
@@ -11,4 +11,21 @@ $(function() {
     const videoFrame = $('#video-frame');
     const videoFrameHide = $('#video-frame-hide');
     const embedVideo = $('#embed-video');
+
+    // initialize hero slider
+    $('#hero-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll:1,
+        arrows: false,
+        dots:true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnFocus:false,
+        pauseOnHover: false,
+        fade:true,
+        speed:1000,
+        cssEase: 'linear',
+    });
+    
 });
