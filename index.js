4db16f47f347fe1fdf0590d0da04e28e91e27cd2
emilywilbur <ewilbur94@gmail.com>
2022-01-25 14:18:09 -0500

    stored jquery references in const variables

---
+++ index.js
@@ -0,0 +1,14 @@
+// document ready function
+
+$(function() {
+    // store jquery references in const variables
+    const headerNav = $('header nav');
+    const hamburgerMenu = $('#hamburger-menu');
+    const search = $('#search');
+    const searchContainerHide = $('#search-container-hide');
+    const searchContainer = $('#search-container');
+    const youtubeButton = $('.youtube-button');
+    const videoFrame = $('#video-frame');
+    const videoFrameHide = $('#video-frame-hide');
+    const embedVideo = $('#embed-video');
+});
